ea77a1a3894a0d8a1bb2fd74ce15c1e9c4f0f327
victorias722 <119906318+victorias722@users.noreply.github.com>
2023-04-11 09:23:34 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+
+    public static final int GRAVITY = 1000;
+    public static final int REBOUND_VELOCITY = -700;
+
+
+    public static final int DURATION = 16;
+
+	  public static final int PLATFORM_WIDTH = 50; 
+	  public static final int PLATFORM_HEIGHT = 20; 
+	  public static final int DOODLE_WIDTH = 25; 
+	  public static final int DOODLE_HEIGHT = 50;
+
+}
